09f173b08cfaebf1f53ca2c2645b8ca1e62dfe0f
PatrickYuen <ply209@nyu.edu>
2017-04-23 04:05:42 -0400

    First working version of local streaming word cloud

---
+++ app.py
@@ -0,0 +1,122 @@
+import twilio.twiml
+import datetime
+import sys
+import json
+import requests
+import praw
+import random
+import string
+from nltk.corpus import stopwords
+
+#deal with words
+translator=str.maketrans('','',string.punctuation)
+stop = set(stopwords.words('english'))
+
+from flask import Flask, request, redirect, Response, jsonify, render_template
+#from apscheduler.schedulers.background import BackgroundScheduler
+from twilio.rest import Client
+
+app = Flask(__name__)
+
+# put your own credentials here
+account_sid = "AC20f7ada53a2d3e4c21d9efcaba259cf7"
+auth_token = "dc553ee77c173821f6f534ce7dd505bb"
+twilio_num = "+19083605630"
+
+clientID = 'tMDwX9bW48UbFw'
+clientSecret = 'WVIX_qRYMi0knQBTBCszYwf_uzY'
+
+client = Client(account_sid, auth_token)
+
+word_counts = {} #{"Hello": 1, "test": 2, "Christmas": 5, "Pizza": 5, "Cya": 3, "Jingle": 4, "Mongo": 1, "Calzone": 3, "Madlyfe": 2}
+rec_prompts = [] #["Test", "Test1", "Test2", "Test3", "Test4"]
+
+prompt_len = 0
+
+wp_subreddit = praw.Reddit(client_id=clientID,
+				 client_secret=clientSecret,
+				 user_agent='testscript').subreddit('WritingPrompts')		
+
+#Add Sentence to word_counts
+def word_counts_update(sent):
+
+	#Replace punc
+	sent_no_punc = sent.translate(translator)
+
+	#Count important words
+	for word in sent_no_punc.lower().split():
+		if word.lower() not in stop:
+			word_counts[word] = 1 if word.lower() not in word_counts else word_counts[word] + 1
+		
+
+@app.route("/getPrompt", methods=['POST'])
+def getPrompt():
+	global prompt_len
+	"""Respond to incoming calls with a simple text message."""
+
+	resp = twilio.twiml.Response()
+	
+	idea = request.values['Body'].lower()
+	
+	prompt = get_random_wp(idea)
+	
+	#Get Rid of Stopwords, clean text and populate dictionaries
+	word_counts_update(idea)
+	word_counts_update(prompt[4:])
+	
+	prompt_len += len(prompt)
+	
+	#Append Prompt
+	rec_prompts.append(prompt)
+	
+	resp.message(prompt)
+	
+	print(str(resp))	
+	
+	return str(resp)
+	
+def get_random_wp(info):
+	posts = [x for x in wp_subreddit.search(query=info, limit=5) if x.title.startswith('[WP]')]			
+				
+	if len(posts) == 0:
+		return "Your idea is too weird ..."
+		
+	chosen_post = random.choice(posts).title
+	
+	return chosen_post
+
+@app.route("/")
+@app.route("/main")
+def main():
+	print(word_counts)
+	return render_template('index.html')
+	
+@app.route('/getData', methods=['GET'])
+def getData():
+	global word_counts 
+	global rec_prompts
+	global prompt_len
+
+	#WordCloud cache
+	wc_output = list(map(lambda key: {"text": key, "size": 10 + word_counts[key] * 10}, word_counts.keys()))
+	
+	output = { "rp_output": rec_prompts[:4], "wc_output": wc_output }
+	
+	limit = 3
+	
+	#Clear Caches
+	if len(rec_prompts) > limit or prompt_len > 300:
+		word_counts = {} 
+		rec_prompts = rec_prompts[-(min(len(rec_prompts) - 1, limit - 1)):]
+		for sent in rec_prompts:
+			word_counts_update(sent[4:])
+	
+	return json.dumps(output);
+	
+#This is the page for our game	
+@app.route('/showSignUp')
+def showSignUp():
+	return render_template('signup.html')
+	
+if __name__ == "__main__":
+	app.run(debug=True)
---
+++ static/js/wordcloud_v2.js
@@ -0,0 +1,109 @@
+function wordCloud(selector) {
+
+    var fill = d3.scale.category20();
+
+    //Construct the word cloud's SVG element
+    var svg = d3.select(selector).append("svg")
+        .attr("width", 300)
+        .attr("height", 300)
+        .append("g")
+        .attr("transform", "translate(150,150)");
+
+
+    //Draw the word cloud
+    function draw(words) {
+        var cloud = svg.selectAll("g text")
+                        .data(words, function(d) { return d.text; })
+
+        //Entering words
+        cloud.enter()
+            .append("text")
+            .style("font-family", "Impact")
+            .style("fill", function(d, i) { return fill(i); })
+            .attr("text-anchor", "middle")
+            .attr('font-size', 1)
+            .text(function(d) { return d.text; });
+
+        //Entering and existing words
+        cloud
+            .transition()
+                .duration(600)
+                .style("font-size", function(d) { return d.size + "px"; })
+                .attr("transform", function(d) {
+                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                })
+                .style("fill-opacity", 1);
+
+        //Exiting words
+        cloud.exit()
+            .transition()
+                .duration(200)
+                .style('fill-opacity', 1e-6)
+                .attr('font-size', 1)
+                .remove();
+    }
+
+
+    //Use the module pattern to encapsulate the visualisation code. We'll
+    // expose only the parts that need to be public.
+    return {
+
+        //Recompute the word cloud for a new set of words. This method will
+        // asycnhronously call draw when the layout has been computed.
+        //The outside world will need to call this function, so make it part
+        // of the wordCloud return value.
+        update: function(words) {
+            d3.layout.cloud().size([300, 300])
+                .words(words)
+                .rotate(function() { return ~~(Math.random() * 2) * 90; })
+                .font("Impact")
+                .fontSize(function(d) { return d.size; })
+                .on("end", draw)
+                .start();
+        }
+    }
+
+}
+
+function updatePrompts(recPrompts) {
+	$('#PromptList').hide().fadeOut( 1500, function() {
+		$('#PromptList').remove();
+		
+		var ul = $("<ul id=\"PromptList\" class=\"text-left\" style=\"margin-top:20px;padding-left:5px;\">" );
+		// iterate over the array and build the list
+		for (var i = 0; i < recPrompts.length; ++i) {
+			ul.append("<li style=\"margin:5px;\">" + recPrompts[i] + "</li>");
+		}
+		
+		$( "#RecPrompts" ).append(ul);
+		ul.hide().fadeIn( 800 );
+	});
+}
+
+//This method tells the word cloud to redraw with a new set of words.
+//In reality the new words would probably come from a server request,
+// user input or some other source.
+function showNewWords(vis) {
+	$.ajax({
+		url: '/getData',
+		type: 'GET',
+		success: function(response) {
+			
+			//Update Recent Prompts
+			updatePrompts(JSON.parse(response).rp_output)
+			
+			//Update WordCloud
+			vis.update(JSON.parse(response).wc_output)
+			setTimeout(function() { showNewWords(vis) }, 5000)
+		},
+		error: function(error) {
+			console.log(error);
+		}
+	});
+}
+
+//Create a new instance of the word cloud visualisation.
+var myWordCloud = wordCloud('#WordCloud');
+
+//Start cycling through the demo data
+showNewWords(myWordCloud);
